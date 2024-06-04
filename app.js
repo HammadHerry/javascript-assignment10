b01412c62f14c9c534adc4e8dcce30377ca9cbba
HammadHerry <hammadherry81@gmail.com>
2024-06-04 07:02:13 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,97 @@
+// var h = prompt("is that cat");
+// if(h==="yes"){
+//     alert("this is a taybo");
+// }
+// else{
+//     alert("no na nhi not");
+// }
+
+
+// var g = prompt("Are you client");
+// if(g==="yes"){
+//     alert("welcome to web world");
+// }
+// else{
+//     alert("phele fursat me nikal");
+// }
+
+
+
+// var i = prompt("is that software house");
+// if(i==="yes"){
+// alert("welcome");
+// }
+// else{
+//     alert("hamara time waste mat karen");
+
+// }
+
+
+
+// var z = prompt("sir hamza are you there");
+// if(z==="yes"){
+//     alert("g ajaen");
+// }
+// else{
+//     alert("nhi meri rooh aj paraegi");
+// }
+
+
+
+// var w = prompt("whom is sir jamshed");
+// if(w==="yes"){
+//     alert("he is the greatest teacher and kind hearted person");
+// } 
+// else{
+//     alert("pata nhi hum to abi abi gaon se aye hen")
+// }
+
+
+// var u = prompt("who is imran zain sir");
+// if("u===yes"){
+//     alert("he is the teacher of hammad");
+// }
+
+// else{
+//     alert("software house owner");
+// }
+
+
+
+// var b = prompt("who is sir zubair");
+// if(b==="no idea"){
+//     alert("he is the founder of auj institute");
+// }
+// else{
+//     alert("ja kar puch len");
+// }
+
+
+// var c = prompt("who is hammad");
+// if(c==="no idea"){
+//     alert("he is the web developer");
+// }
+// else{
+//     alert("ajao me apko detail me batata hn");
+// }
+
+
+
+// var q = prompt("who is charlie champlen");
+// if(q==="pta nhi"){
+//     alert("funkar tha");
+
+// }
+// else{
+//     alert("books par kar pata chal jayga");
+// }
+
+var t = prompt("led or lcd me kia deffrence hy");
+if(t==="ap bataen"){
+    alert("ek patli or ek smart hoti hy");
+}
+else{
+    alert(
+        "maaf kar de bhai" 
+    )
+}
